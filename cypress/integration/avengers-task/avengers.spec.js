0174f6876f178ad2560d12e8a53985c23ac883bf
Muraveyko <a.muraveyko@a1qa.com>
2022-01-31 15:36:10 +0300

    fixes

--- cypress/integration/avengers-task/avengers.spec.js
+++ cypress/integration/avengers-task/avengers.spec.js
@@ -1,64 +1,100 @@
 /// <reference types="cypress" />
 
 describe('checking `the movie database` web-site`', () => {
-  const link = 'https://skempin.github.io/reactjs-tmdb-app/'
-  beforeEach(() => {
-    cy.visit(link)
-  })
-
- it('logo and url are correct', () => {
-    cy.url().should('include', 'reactjs-tmdb-app/')
+  it('logo and url are correct', () => {
+    cy.visit('/');
+    cy.url().should('include', 'reactjs-tmdb-app/');
     cy.get('.logo')
+      .should('exist')
+      .should('be.visible')
       .should('have.attr', 'alt', 'The Movie Database')
-      .should('have.attr', 'src', './images/tmdb.svg')
-  })
+      .should('have.attr', 'src', './images/tmdb.svg');
+  });
 
   it('can type in search bar', () => {
-    const newSearchValue = 'Arrival'
-    cy.get('#q').type(`${newSearchValue}{enter}`)
-    cy.get('.tt-suggestions strong')
-      .should('have.length', 5)
-      .should('contain', newSearchValue)
-    const newSearchResults = ['The ', 'The Second ']
-    newSearchResults.forEach( (searchResults) => {
-      cy.get('.tt-suggestions p')
-        .should('contain', searchResults)
-        .children()
-        .should('contain', newSearchValue)
-    })
-  })
+    const newSearchValue = 'Arrival';
+    cy.intercept({
+      method: 'GET',
+      url: 'https://api.themoviedb.org/3/search/movie?*',
+    }).as('getSearchMovies');
+
+    cy.searchMovies(newSearchValue);
+
+    cy.wait('@getSearchMovies');
+
+    cy.get('.tt-suggestions p').should('have.length', 5);
+    // .should('contain', newSearchValue);
+
+    // const newSearchResults = ['The ', 'The Second '];
+
+    // newSearchResults.forEach((searchResults) => {
+    //   cy.get('.tt-suggestions p')
+    //     .should('contain', searchResults)
+    //     // .children()
+    //     .should('contain', newSearchValue);
+    // });
+
+    cy.get('.tt-suggestions p')
+      .then(($items) => Array.from($items).map((i) => i.innerText))
+      .should((lebels) => {
+        expect(lebels).to.include('Arrival');
+        expect(lebels).to.include('The Arrival');
+        expect(lebels).to.include('The Second Arrival');
+      });
+  });
 
   it('can select search result', () => {
-    const newSearchValue = 'The Arrival'
-    cy.get('#q').type(`${newSearchValue}{enter}`)
-    cy.get('.tt-suggestions p')
-      .eq(4)
-      .click()
+    const newSearchValue = 'Arrival';
+    cy.searchMovies(newSearchValue);
+    cy.get('.tt-suggestions p').eq(0).click();
     //unstable search result (can be one of 3)
-    const taglineOne = 'Mommy!'
-    const taglineTwo = 'Your destination is in your dream.'
-    const taglineTree = 'Mankind was born on Earth. It was never meant to die here.'
-    const taglinesArray = [taglineOne, taglineTwo, taglineTree]
-    cy.get('.tagline').invoke('text').then(text =>
-      expect(text).to.be.oneOf(taglinesArray)
-    )
-  })
+    const expectedMoto = 'Why are they here?';
+
+    cy.get('h1').should('have.text', newSearchValue);
+    cy.get('.tagline').should('have.text', expectedMoto);
+    // .invoke('text')
+    // .then((text) => expect(text).to.be.oneOf(taglinesArray));
+  });
+
+  it('can select search result', () => {
+    const newSearchValue = 'The Arrival';
+
+    cy.intercept({
+      method: 'GET',
+      url: 'https://api.themoviedb.org/3/search/movie?*',
+    }).as('getSearchMovies');
+
+    cy.searchMovies(newSearchValue);
+
+    cy.wait('@getSearchMovies');
+
+    cy.get('.tt-suggestions p').eq(3).click();
+    const taglineOne = 'The battle for Earth has begun';
+
+    cy.get('.tagline').should('have.text', taglineOne);
+  });
 
   it('can change response', () => {
-    const theAvengersMovieInfo = 'https://api.themoviedb.org/3/movie/24428?&api_key=cfe422613b250f702980a3bbf9e90716'
-    const newVoteAverage = '101.1'
+    const theAvengersMovieInfo = 'https://api.themoviedb.org/3/movie/*';
+
+    const newVoteAverage = '101.1';
+
     cy.intercept(theAvengersMovieInfo, (req) => {
-      req.reply(res => {
-        res.body.vote_average = newVoteAverage
-      })
-    })
-    const newSearchValue = 'The Avengers'
-    cy.get('#q').type(`${newSearchValue}{enter}`)
-    cy.get('.tt-suggestions p')
-      .eq(0)
-      .click()
-    cy.get(':nth-child(4) > .meta-data').should('contain', newVoteAverage)
+      req.reply((res) => {
+        res.body.vote_average = newVoteAverage;
+      });
+    }).as('getMovie');
 
-  })
+    const newSearchValue = 'The Avengers';
 
-})
+    cy.searchMovies(newSearchValue);
+    cy.get('.tt-suggestions p').eq(0).click();
+
+    cy.wait('@getMovie').then((xhr) => console.log(xhr));
+
+    cy.get('div:contains("Aver") > .meta-data').should(
+      'contain',
+      newVoteAverage
+    );
+  });
+});
